d952c99c23e1bbf18281482c3763bc28238a67f5
m.safavi <m.safavi@nisys.ir>
2022-10-10 18:38:41 +0330

    create docker file and host on docker

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 #build stage
-FROM golang:alpine AS builder
-RUN apk add --no-cache git
-WORKDIR /go/src/app
-COPY . .
-RUN go get -d -v ./...
-RUN go build -o /go/bin/app -v ./...
+FROM golang:alpine 
+COPY ./ /go/src/shorturllinkapi
+#RUN apk add --no-cache git
+WORKDIR /go/src/shorturllinkapi
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download
 
-#final stage
-FROM alpine:latest
-RUN apk --no-cache add ca-certificates
-COPY --from=builder /go/bin/app /app
-ENTRYPOINT /app
-LABEL Name=shorturllinkapi Version=0.0.1
+COPY *.go  ./
+RUN go build -o /shorturllinkapi
 EXPOSE 3000
+CMD ["/shorturllinkapi"]
--- database/config.go
+++ database/config.go
@@ -5,9 +5,14 @@ import "fmt"
 var (
 	dbUsername = "postgres"
 	dbPassword = "postgres"
-	dbHost     = "localhost"
-	dbTable    = "postgres"
+	dbHost     = "shortlinkdb"
+	dbDB       = "postgres"
 	dbPort     = "5432"
-	pgConnStr  = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	pgConnStr  = fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		dbUsername, dbPassword, dbHost, dbPort, dbDB)
+	// pgConnStr  = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+	// 	dbHost, dbPort, dbUsername, dbTable, dbPassword)
 )
+
+// pgConnStr  = fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+// dbUsername, dbPassword, dbHost, dbPort, dbDB)
